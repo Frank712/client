00fdec86c0dbd757b5051208387a330b5762f746
Francisco Lino <franksuriel_7@hotmail.com>
2021-07-07 06:18:45 -0500

    ADD routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import "./App.css";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-const App = () => (
-  <div>
-    <p>React App</p>
-  </div>
-);
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Switch>
+  );
+};
 
 export default App;
